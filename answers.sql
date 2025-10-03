7e2271bf4ed9f655db6e8372c86849e6157a9f67
Caleb020 <chriscaleb2005@gmail.com>
2025-10-03 18:56:49 +0000

    answers.sql

---
+++ answers.sql
@@ -0,0 +1,8 @@
+//question one
+DROP INDEX IdxPhone ON customers;
+//question two
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+//question three
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+//question four
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
